2203695a0327d359605afa5c7a3c1298d79c54f6
DeeGardner <gar21018@byui.edu>
2023-09-21 17:30:41 -0600

    Week2.2

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -5,7 +5,7 @@
 /* Step 2 - Variables */
 const fullName = "Samuel Dee Gardner";
 let currentYear = new Date().getFullYear();
-let profilePicture = "images/me.PNG";
+let profilePicture = "images/me.png";
 
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById("name");
