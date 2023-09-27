b056d4142961c9150bae61ef8c0862a6496b1e14
DeeGardner <gar21018@byui.edu>
2023-09-27 09:42:26 -0600

    Wk2

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -5,7 +5,7 @@
 /* Step 2 - Variables */
 const fullName = "Samuel Dee Gardner";
 let currentYear = new Date().getFullYear();
-let profilePicture = "https://github.com/DeeGardner/cse121b/tree/main/images";
+let profilePicture = "images/me2.png";
 
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById("name");
